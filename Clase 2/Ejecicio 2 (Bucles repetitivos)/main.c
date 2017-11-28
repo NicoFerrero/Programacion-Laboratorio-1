a6599fa86350f1a370bab8bf454520b149400f5c
Nicolás Alejandro Ferrero <nicolasferreroutn@gmail.com>
2017-11-27 21:01:53 -0300

    Add files via upload

---
+++ Clase 2/Ejecicio 2 (Bucles repetitivos)/main.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/*
+Se ingresan numeros enteros (positivos), no se sabe cuantos.
+Determinar:
+ - cantidad de numeros comprendidos entre 10 y 100 (inclusive) (X)
+ - cantidad de numeros primos ()
+ - cantidad de numeros pares (X)
+ - cantidad de numeros impares (X)
+ - promedio (X)
+ - maximo (X)
+ - minimo (X)
+
+*/
+
+
+
+int main()
+{
+    int numero;
+    int cont10a100 = 0;
+    int contPares = 0;
+    int contImpares = 0;
+    int contador = 0;
+    int acumulador = 0;
+    float promedio;
+    int maximo;
+    int minimo;
+    int flag = 0;
+    int i;
+    int esPrimo = 0;
+    int contPrimos = 0;
+    char seguir;
+
+    do
+    {
+        printf("\nIngrese un numero: ");
+        scanf("%d", &numero);
+        while(!(numero>0))
+        {
+            printf("Error!!! Ingrese un numero positivo: ");
+            scanf("%d", &numero);
+        }
+
+        if(numero>=10 && numero<=100)
+        {
+            cont10a100++;
+        }
+        if(numero%2==0)
+        {
+            contPares++;
+        }
+        else
+        {
+            contImpares++;
+        }
+
+        acumulador += numero;//acumulador = acumulador + numero;
+        contador++;
+
+        if(flag==0 || numero>maximo)
+        {
+            maximo = numero;
+        }
+        if(flag==0 || numero<minimo)
+        {
+            minimo = numero;
+            flag = 1;
+        }
+
+        /*
+        if(flag==0)
+        {
+            maximo = numero;
+            minimo = numero;
+            flag = 1;
+        }
+        else
+        {
+            if(numero>maximo)
+            {
+                maximo = numero;
+            }
+            if(numero<minimo)
+            {
+                minimo = numero;
+            }
+        }*/
+
+        esPrimo = 0;
+        for(i=1; i<=numero; i++)
+        {
+            if(numero%i==0)
+            {
+               esPrimo++;
+            }
+        }
+        if(esPrimo==2)
+        {
+            contPrimos++;
+        }
+
+        printf("Desea ingresar otro numero? (s/n): ");
+        seguir = tolower(getche());
+
+
+
+    }while(seguir=='s');
+
+    promedio = (float)acumulador / contador;
+
+    printf("\nLa cantidad de numeros entre 10 y 100 (inclusive) es: %d\n", cont10a100);
+    printf("La cantidad de pares es: %d\n", contPares);
+    printf("La cantidad de impares es: %d\n", contImpares);
+    printf("El promedio es: %.2f\n", promedio);
+    printf("El maximo es %d, y el minimo es %d\n", maximo, minimo);
+    printf("Cont primos: %d", contPrimos);
+
+
+    return 0;
+}
+
+
+
+
+ /*while(cont<10)
+    {
+        cont++;
+        printf("%d\n", cont);
+    }
+
+   do
+    {
+        cont++;
+        printf("%d\n", cont);
+    }while(cont<10);*/
+
+    /*for(inicializacion de la variable de control; condicion de continuidad de bucle; incremento de la variable de control)*/
+
+    /*for(cont=0; cont<10; cont++)
+    {
+        printf("%d\n", cont+1);
+    }*/
